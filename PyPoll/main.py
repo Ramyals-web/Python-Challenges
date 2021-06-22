3591d55bccc0872a511423c00052a9c616d84043
Ramya LS <ramya.sivaprakasam@gmail.com>
2021-06-22 00:22:11 -0400

    Final Homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,19 +21,19 @@ with open(myfile) as csv_file:
     for row in csvreader:
        # print(row)
         total = total+1
-        #votes.append(cast_votes)
         candidate=(row[2])
         candidates.append(row[2])
+    
     for candidate in candidates:
+
         if candidate == "Khan":
-         khan= khan+1
-        elif candidate == "correy":
+            khan= khan+1
+        elif candidate == "Correy":
             correy= correy+1
-         
-        elif candidate == "li":
-         li= li+1
-        elif candidate== "otooley":
-         otooley= otooley+1
+        elif candidate == "Li":
+            li= li+1
+        elif candidate== "O'Tooley":
+            otooley= otooley+1
     
     khan_percentage = (khan/total)*100
     correy_percentage = (correy/total)*100
@@ -42,13 +42,13 @@ with open(myfile) as csv_file:
 
 
     if khan  > max(correy, li, otooley):
-         winner = "khan"
+        winner = "Khan"
     elif correy > max(khan, li, otooley):
-            winner = "correy"
+        winner = "Correy"
     elif li > max(khan, correy, otooley):
-         winner = "li"
+         winner = "Li"
     else:
-        winner = "otooley"
+        winner = "O'Tooley"
     
     
 
@@ -58,8 +58,8 @@ with open(myfile) as csv_file:
     f"--------------------------------\n"
     f" Khan:{khan_percentage:.2f}% which is {khan} votes\n"
     f" Correy:{correy_percentage:.2f}% which is {correy} votes\n"
-    f" li: {li_percentage:.2f}% which is {li} votes\n"
-    f" OTooley: {otooley_percentage:.2f}% which is {otooley} votes\n"
+    f" Li: {li_percentage:.2f}% which is {li} votes\n"
+    f" O'Tooley: {otooley_percentage:.2f}% which is {otooley} votes\n"
     f"--------------------------------\n"
     f" Winner: {winner:}\n"
     f"--------------------------------\n")
